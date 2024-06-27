7586874cd0bb008c4f8dd55e4ce3b3206038ba3a
Ramzy842 <ramzychahbani@gmail.com>
2024-06-27 13:38:03 +0100

    ex01 init

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+class BitcoinExchange
+{
+private:
+    /* data */
+public:
+    BitcoinExchange();
+    ~BitcoinExchange();
+};
+
+
+
+#endif
