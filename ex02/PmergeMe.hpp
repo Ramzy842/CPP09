7c4cdfa39377114c70afaccd1231d58d6fa39a39
Ramzy842 <ramzychahbani@gmail.com>
2024-06-30 19:57:50 +0100

    fixed makefile and init ex02

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,10 @@
+class PmergeMe
+{
+    private:
+    
+    public:
+        PmergeMe();
+        ~PmergeMe();
+        PmergeMe(const PmergeMe &original);
+        PmergeMe &operator=(const PmergeMe &original);
+};
